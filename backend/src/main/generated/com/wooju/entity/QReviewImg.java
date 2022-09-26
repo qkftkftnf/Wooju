1cffd136bead90e8ae5ba224c8204a786342539a
박상수 <qkftkftnf@naver.com>
2022-09-26 14:32:01 +0900

    💊[CHORE] create QClass

---
+++ backend/src/main/generated/com/wooju/entity/QReviewImg.java
@@ -0,0 +1,56 @@
+package com.wooju.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReviewImg is a Querydsl query type for ReviewImg
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QReviewImg extends EntityPathBase<ReviewImg> {
+
+    private static final long serialVersionUID = -1297701831L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReviewImg reviewImg = new QReviewImg("reviewImg");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final StringPath img = createString("img");
+
+    public final QReview review;
+
+    public final QUser user;
+
+    public QReviewImg(String variable) {
+        this(ReviewImg.class, forVariable(variable), INITS);
+    }
+
+    public QReviewImg(Path<? extends ReviewImg> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReviewImg(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReviewImg(PathMetadata metadata, PathInits inits) {
+        this(ReviewImg.class, metadata, inits);
+    }
+
+    public QReviewImg(Class<? extends ReviewImg> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.review = inits.isInitialized("review") ? new QReview(forProperty("review"), inits.get("review")) : null;
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
+    }
+
+}
+
