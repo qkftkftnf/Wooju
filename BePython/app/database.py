d0dd1dad7ec9db22363873cdbddf6c31d71b9e87
Woozzang <dannba1@naver.com>
2022-09-28 13:27:51 +0900

    [fix] bimil . json

--- BePython/app/database.py
+++ BePython/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-with open('./secrets.json', 'r', encoding='utf-8') as f:
+with open('secrets.json', 'r', encoding='utf-8') as f:
     secrets = json.load(f)
 
 MYSQL_URL = f'mysql+pymysql://{secrets["USERNAME"]}:{secrets["PASSWORD"]}@{secrets["HOSTNAME"]}:{secrets["PORT"]}/{secrets["DBNAME"]}'
