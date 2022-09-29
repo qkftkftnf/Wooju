b3e990f3ce506ab9545ee44954740da68cf4e12d
HannaJang <letnumdot124@gmail.com>
2022-09-29 10:02:02 +0900

    frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import CommunityMain from "../views/community/CommunityMain.vue";
 import CommunityHotReview from "../views/community/CommunityHotReview.vue";
-import WoojooSearch from "../views/woojoo_search/WoojooSearch.vue"
-import MyPageMain from "../views/mypage/MyPageMain.vue";
+// import WoojooSearch from "../views/woojoo_search/WoojooSearch.vue"
+// import MyPageMain from "../views/mypage/MyPageMain.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,16 +18,16 @@ const router = createRouter({
       path: "/hotreview",
       component: CommunityHotReview,
     },
-    {
-      name: "WoojooSearch",
-      path: "/Woojoo",
-      component: WoojooSearch,
-    },
-    {
-      name: "MyPageMain",
-      path: "/id",
-      component: MyPageMain,
-    }
+    // {
+    //   name: "WoojooSearch",
+    //   path: "/Woojoo",
+    //   component: WoojooSearch,
+    // },
+    // {
+    //   name: "MyPageMain",
+    //   path: "/id",
+    //   component: MyPageMain,
+    // }
   ],
 });
 
