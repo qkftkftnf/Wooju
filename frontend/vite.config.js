5c85fc006c72636d359aa9284f3847f1fcb4e715
김우창 <dannba1@naver.com>
2022-09-29 10:11:03 +0900

    141243

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
