1451f83396f80d13ed40c6b23e24c95ad3e93647
HannaJang <letnumdot124@gmail.com>
2022-09-29 10:17:59 +0900

    frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="app-container">
-    <!-- <HeaderView/> -->
-    <RouterView/>
-    <MenuView/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script setup>
-import { RouterView } from "vue-router";
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-import HeaderView from "@/views/common/HeaderView.vue"
-import MenuView from "@/views/common/MenuView.vue"
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-@import "assets/css/common.css";
-@import "assets/css/CommunityMain.css";
-@import "assets/css/CommunityHot.css";
-@import "assets/css/WoojooSearch.css";
-@import "assets/css/myPage.css";
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-import './style.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/display.css'
-
-
-createApp(App)
-  .use(router)
-  .use(store)
-  .use(ElementPlus)
-  .mount('#app');
+createApp(App).mount('#app')
