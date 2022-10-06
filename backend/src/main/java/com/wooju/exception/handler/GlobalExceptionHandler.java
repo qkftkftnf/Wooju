eb5ab52130c55871326088bcc6af6cb0ac1e2201
박상수 <qkftkftnf@naver.com>
2022-10-06 16:48:18 +0900

    💊[CHORE] modify error code

--- backend/src/main/java/com/wooju/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/wooju/exception/handler/GlobalExceptionHandler.java
@@ -22,12 +22,12 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ReviewNotFoundException.class)
     protected ResponseEntity<BaseResponseDto> handleReviewNotFoundException(ReviewNotFoundException e) {
-        return ResponseEntity.status(403).body(BaseResponseDto.of(402, "Review Not Found"));
+        return ResponseEntity.status(402).body(BaseResponseDto.of(402, "Review Not Found"));
     }
 	
 	@ExceptionHandler(ProductNotFoundException.class)
     protected ResponseEntity<BaseResponseDto> handleProductNotFoundException(ProductNotFoundException e) {
-        return ResponseEntity.status(403).body(BaseResponseDto.of(406, "Product Not Found"));
+        return ResponseEntity.status(406).body(BaseResponseDto.of(406, "Product Not Found"));
     }
 	
 	@ExceptionHandler(LikeException.class)
@@ -37,11 +37,11 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ExistUserException.class)
     protected ResponseEntity<BaseResponseDto> handleExistUserException(ExistUserException e) {
-        return ResponseEntity.status(404).body(BaseResponseDto.of(407, "User Exist"));
+        return ResponseEntity.status(407).body(BaseResponseDto.of(407, "User Exist"));
     }
 	
 	@ExceptionHandler(DifferentUserException.class)
     protected ResponseEntity<BaseResponseDto> handleDifferentUserException(DifferentUserException e) {
-        return ResponseEntity.status(404).body(BaseResponseDto.of(408, "User Different"));
+        return ResponseEntity.status(408).body(BaseResponseDto.of(408, "User Different"));
     }
 }
